167cbbaf5dcd6b822023ceff5123f2be33eca301
kitzbergerg <gabriel.kitzberger@gmail.com>
2023-04-06 19:42:14 +0200

    add config parser, reformat

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::{
     ffi::OsStr,
     fs::File,
-    io::{ErrorKind, Write},
+    io::{BufRead, BufReader, ErrorKind, Write},
     path::PathBuf,
     str::FromStr,
     vec,
@@ -14,7 +14,37 @@ use mdbook::{
     MDBook,
 };
 
-/// A no-op preprocessor.
+/// Possible configuration options when running the preprocessor
+struct Config {
+    /// Use the first line of the file and parse '# <chapter_name>' if set. Defaults to false.
+    get_chapter_name_from_file: bool,
+    /// The file to use for chapters with children. Defaults to 'README'.
+    /// Do not include the file extension as it will be '.md' anyways.
+    chapter_file_name: String,
+    /// Creates empty file with name chapter_file_name if it is missing in a directory. Defaults to
+    /// false.
+    /// When false the preprocessor panics if the file is <chapter_file_name>.md is missing in a
+    /// directory.
+    create_missing_chapter_files: bool,
+}
+
+impl From<&toml::map::Map<String, toml::value::Value>> for Config {
+    fn from(value: &toml::map::Map<String, toml::value::Value>) -> Self {
+        Self {
+            get_chapter_name_from_file: value
+                .get("get_chapter_name_from_file")
+                .map_or(false, |val| val.as_bool().unwrap()),
+            chapter_file_name: value
+                .get("chapter_file_name")
+                .map_or("README".to_owned(), |val| val.as_str().unwrap().to_owned()),
+            create_missing_chapter_files: value
+                .get("create_missing_chapter_files")
+                .map_or(false, |val| val.as_bool().unwrap()),
+        }
+    }
+}
+
+#[derive(Debug, Default)]
 pub struct GenerateSummary;
 
 impl GenerateSummary {
@@ -25,17 +55,11 @@ impl GenerateSummary {
 
 impl Preprocessor for GenerateSummary {
     fn name(&self) -> &str {
-        "generate-summary-preprocessor"
+        "generate-summary"
     }
 
     fn run(&self, ctx: &PreprocessorContext, _: Book) -> Result<Book, Error> {
-        // In testing we want to tell the preprocessor to blow up by setting a
-        // particular config value
-        if let Some(nop_cfg) = ctx.config.get_preprocessor(self.name()) {
-            if nop_cfg.contains_key("blow-up") {
-                anyhow::bail!("Boom!!1!");
-            }
-        }
+        let config = Config::from(ctx.config.get_preprocessor(self.name()).unwrap());
 
         let book_dir = &ctx.root.join(&ctx.config.book.src);
 
@@ -51,7 +75,7 @@ impl Preprocessor for GenerateSummary {
         let summary = Summary {
             title: Option::None,
             prefix_chapters: vec![],
-            numbered_chapters: generate_chapters(book_dir, &SectionNumber::default()),
+            numbered_chapters: generate_chapters(book_dir, &SectionNumber::default(), &config),
             suffix_chapters: vec![],
         };
 
@@ -67,8 +91,75 @@ impl Preprocessor for GenerateSummary {
     }
 }
 
-fn generate_chapters(dir_path: &PathBuf, section: &SectionNumber) -> Vec<SummaryItem> {
-    let mut entries = std::fs::read_dir(dir_path)
+fn generate_chapters(
+    dir_path: &PathBuf,
+    section: &SectionNumber,
+    config: &Config,
+) -> Vec<SummaryItem> {
+    let mut entries = get_markdown_files_and_directories(dir_path);
+
+    // Sort by filename
+    entries.sort_by_key(|a| a.file_name());
+
+    entries
+        .into_iter()
+        .enumerate()
+        .filter_map(|(i, entry)| {
+            let path = entry.path();
+            let name = path.file_stem().unwrap().to_str().unwrap().to_owned();
+
+            let mut section = section.clone();
+            section.push(i as u32);
+
+            let link = if entry.file_type().unwrap().is_file() {
+                // files are used by chapter headings
+                if name.as_str() == config.chapter_file_name {
+                    return None;
+                }
+
+                Link {
+                    name: if config.get_chapter_name_from_file {
+                        get_chapter_name_from_file(&path)
+                    } else {
+                        name
+                    },
+                    location: Some(path),
+                    nested_items: vec![],
+                    number: Some(section),
+                }
+            } else {
+                let mut chapter_readme = path.clone();
+                chapter_readme
+                    .push(PathBuf::from_str(&format!("{}.md", config.chapter_file_name)).unwrap());
+
+                if !chapter_readme.exists() {
+                    if config.create_missing_chapter_files {
+                        let mut file = File::create(&chapter_readme).unwrap();
+                        write!(file, "# {}.md", config.chapter_file_name).unwrap();
+                    } else {
+                        panic!("Missing chapter file: {:?}", chapter_readme);
+                    }
+                }
+
+                Link {
+                    name,
+                    location: Some(chapter_readme),
+                    nested_items: generate_chapters(&path, &section, config),
+                    number: Some(section),
+                }
+            };
+
+            Some(SummaryItem::Link(link))
+        })
+        .collect()
+}
+
+/// Get all markdown files and directories in the specified directory. Ignore all other files.
+///
+/// # Panics
+/// If [`std::fs::read_dir`] fails.
+fn get_markdown_files_and_directories(dir_path: &PathBuf) -> Vec<std::fs::DirEntry> {
+    std::fs::read_dir(dir_path)
         .unwrap()
         .map(|entry| entry.unwrap())
         .filter(|entry| {
@@ -84,46 +175,21 @@ fn generate_chapters(dir_path: &PathBuf, section: &SectionNumber) -> Vec<Summary
                 file_type.is_dir()
             }
         })
-        .collect::<Vec<_>>();
-
-    // Sort by filename
-    entries.sort_by(|a, b| a.file_name().cmp(&b.file_name()));
-
-    entries
-        .into_iter()
-        .enumerate()
-        .filter_map(|(i, entry)| {
-            let path = entry.path();
-            let name = path.file_stem().unwrap().to_str().unwrap().to_owned();
-
-            let mut section = section.clone();
-            section.push(i as u32);
-
-            let (location, nested_items) = if entry.file_type().unwrap().is_file() {
-                // README.md files are used by chapter headings
-                if name.as_str() == "README" {
-                    return None;
-                }
-
-                (path.clone(), vec![])
-            } else {
-                let mut chapter_readme = path.clone();
-                chapter_readme.push(PathBuf::from_str("README.md").unwrap());
-
-                if !chapter_readme.exists() {
-                    panic!("Missing chapter file: {:?}", chapter_readme);
-                }
-
-                (chapter_readme, generate_chapters(&path, &section))
-            };
-
-            let link = Link {
-                name,
-                location: Some(location),
-                nested_items,
-                number: Some(section),
-            };
-            Some(SummaryItem::Link(link))
-        })
         .collect()
+}
+
+/// If the first line of the file looks like '# <header>' use the header as the chapter name.
+/// Otherwise use the filename.
+fn get_chapter_name_from_file(path: &PathBuf) -> String {
+    let file = File::open(path).unwrap();
+    let mut reader = BufReader::new(file);
+
+    let mut page_name = String::new();
+    reader.read_line(&mut page_name).unwrap();
+
+    if let Some(stripped) = page_name.strip_prefix("# ") {
+        stripped.to_owned()
+    } else {
+        page_name
+    }
 }
